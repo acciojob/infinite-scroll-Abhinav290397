46ae5a160a39b5e815f53015866472180af1056f
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-28 03:26:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const ScrollContainer = document.querySelector("#infi-list");
+let x=1;
+function addTenElements(){
+	for(let i=0;i<10;i++){
+	const Li = document.createElement("li");
+	Li.innerText = "Item"+" "+x;
+	x++;
+	ScrollContainer.appendChild("Li");
+ }
+}
 
+ScrollContainer.addEventListener("scroll",() =>{
+	const TotalScrollHt = ScrollContainer.scrollHeight;
+	const visibleHt = ScrollContainer.clientHeight;
+	const ScrolledHt = ScrollContainer.ScrollTop;
+	const remainingHtToBeScrolled = TotalScrollHt - (visibleHt+ScrolledHt);
+	if(remainingHtToBeScrolled < 4){
+		addTenElements()
+	}
+});
+addTenElements();
